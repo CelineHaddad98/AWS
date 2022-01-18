7eb23705e13d5cb1ff5d27001b22f9d781d1d66a
celinehaddad98 <celinehaddad98#gmail.com>
2022-01-18 12:11:56 +0200

    create new SNS topic

---
+++ create_topic.py
@@ -0,0 +1,16 @@
+import boto3
+from os import getenv
+ 
+ 
+LOCALSTACK_ENDPOINT_URL = getenv("AWS_ENDPOINT_URL")
+ 
+
+client = boto3.client("sns", endpoint_url=LOCALSTACK_ENDPOINT_URL)
+
+response = client.create_topic(
+    Name='MyTopic'
+)
+
+
+topics = client.list_topics()
+print(topics)
